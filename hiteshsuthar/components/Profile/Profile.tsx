b4696f18478678f1843b81670422bed3167ea20f
Hitesh Suthar <senorhitesh@gmail.com>
2026-09-23 15:37:18 +0530

    'profile-picture-changed-one-last-time'

--- hiteshsuthar/components/Profile/Profile.tsx
+++ hiteshsuthar/components/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import displayPicture from "@/public/profile.jpeg";
+import displayPicture from "@/public/profile.jpg";
 import ShinyText from "../Global/ShineText";
 import { getCalApi } from "@calcom/embed-react";
 
